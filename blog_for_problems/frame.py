731d27c497137723d1a8f98c0476b002753937bb
editor-EDT <81428737+editor-EDT@users.noreply.github.com>
2023-06-28 16:59:03 +0800

    version1.0

---
+++ blog_for_problems/frame.py
@@ -0,0 +1,42 @@
+from xml.etree.ElementTree import Comment
+from flask import Flask,render_template,request
+#from requests import request
+app = Flask(__name__)
+app.debug=True
+
+@app.route('/frame/',methods=["GET","POST"])
+def frame():
+    if request.method == 'POST':
+        print(request.form)
+        if len(request.form) == 1:
+            f = open('infor.txt','a')
+            f.write(request.form['text']+')]}\n')
+            f.close()
+        else:
+            f = open('comments.txt','a')
+            f.write(request.form['id']+'{[('+request.form['comment']+')]}\n')
+            f.close()
+    f = open('infor.txt','r')
+    fricts = f.read().split(')]}\n')[:-1]
+    num_f = [ y for y in range(len(fricts))]
+    print(fricts,num_f)
+    f.close()
+
+    f = open('comments.txt','r')
+    comment_f = f.read().split(')]}\n')[:-1]
+    comments = {}
+    for z in comment_f:
+        try:
+            comments[int(z.split('{[(')[0])].append(z.split('{[(')[1])
+        except:
+            comments[int(z.split('{[(')[0])] = [z.split('{[(')[1]]
+    print(comment_f,comments)
+    f.close()
+    return render_template('/frame.html',posts = fricts, num_f = num_f, comments = comments)
+
+@app.route('/input/')
+def input():
+    return render_template('/input.html')
+
+if __name__ == '__main__':
+    app.run()
